9f7ff9e8b8d8728aaa0fe0d376cbe0535e89e7e3
Paul Dautry <paul.dautry@gmail.com>
2017-07-18 14:12:43 +0200

    Fix security issue
    @SachBar : test the fix and update prod as soon as possible ;)

--- page.php
+++ page.php
@@ -2,13 +2,30 @@
 
 require_once "back/dicts.php"; //définition de $_TRANSLATOR dans ce fichier
 
-function notfound() {
+function notfound($indic) {
 	echo file_get_contents("front/pages/404.html");
+	die("<!--" . $indic . "-->");
+}
+
+function sanitize_path() {
+	$rel_dir = "front/pages/";
+	$pages_dir = realpath($rel_dir);
+	if($pages_dir === FALSE) {
+		notfound("failed 1");
+	}
+	$page = realpath($rel_dir . $_GET['page'] . ".html");
+	if($page === FALSE) {
+		notfound("failed 2");
+	}
+	if(dirname($page) === $pages_dir) {
+		return $page;
+	}
+	notfound("failed 3 - hack attempt detected.");
 }
 
 if(isset($_GET['page']) && isset($_GET['lang'])) {
 	// récupération de la page si elle existe
-	$page = "front/pages/".$_GET['page'].".html";
+	$page = sanitize_path();
 	$header = "front/static/header.html";
 	$footer = "front/static/footer.html";
 	if(file_exists($page)) {
@@ -17,8 +34,8 @@ if(isset($_GET['page']) && isset($_GET['lang'])) {
 		// on réalise le changement de langue
 		echo $_TRANSLATOR->translate($_GET['lang'], $content);
 	} else {
-		notfound();
+		notfound("failed 4 - file not found.");
 	}
 } else {	
-	notfound();
+	notfound("failed 5 - missing parameters.");
 }
