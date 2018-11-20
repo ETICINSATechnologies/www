24e285b5f5e1d1e3c40abdac5c19033c54c9b132
Alexandre <alexandre.lang.06@gmail.com>
2018-11-21 00:06:24 +0100

    Article 5 - RSE

--- back/dicts.php
+++ back/dicts.php
@@ -38,6 +38,7 @@ $links->translate(StringIdentifier::LINK_ARTICLE_1, "page.php?page=voiture_auton
 $links->translate(StringIdentifier::LINK_ARTICLE_2, "page.php?page=jewc");
 $links->translate(StringIdentifier::LINK_ARTICLE_3, "page.php?page=infographie_anciens");
 $links->translate(StringIdentifier::LINK_ARTICLE_4, "page.php?page=caracterisation_thermique");
+$links->translate(StringIdentifier::LINK_ARTICLE_5, "page.php?page=rse_etic");
 
 $links->translate(StringIdentifier::LINK_REA_WEB, "page.php?page=subcategory_websites");
 $links->translate(StringIdentifier::LINK_REA_MOBILE,"page.php?page=subcategory_mobile");
