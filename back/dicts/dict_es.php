bad0d69d20adc23101187f7dd8890924d4ede8d3
lucieMBovo <lucie.bovo@insa-lyon.fr>
2019-02-05 13:31:24 +0100

    -  les images, dans la rubrique Autres prestations pouvant vous interesser, soient aussi cliquables (et image de la page home)  - ajout de la definition des balises {MISSSION} et {MISSION_H1}  - modification des alt="logo" et alt="img" sauf pour voiture autonome, temoignage ancien, events et infographie ancien

--- back/dicts/dict_es.php
+++ back/dicts/dict_es.php
@@ -7,7 +7,3 @@ require_once "back/lang.php";
 // ------------------------------------ DICTIONNAIRE ESPAGNOL ----------------------------------
 $DICT_ES = new Dictionary('es');
 $DICT_ES->translate(StringIdentifier::SID_TITLE, "ETIC INSA Technologies");
-
-$DICT_ES->translate(StringIdentifier::SID_ETIC_RSE_SOUS_TITRE,"Nuestra Visión sobre RSE en ETIC");
-$DICT_ES->translate(StringIdentifier::SID_ETIC_RSE_AUTEUR_DATE, "Por el Consejo de Administracíon | 22 noviembre 2018");
-$DICT_ES->translate(StringIdentifier::SID_ETIC_RSE_P1, "Reinventarse cada día. Ser el actor del futuro dando un paso al frente, pensando en humanidad antes de pensar en la técnica, queriendo la técnica no para fundar una sociedad artificial pero queriendo ella como un impulse del progreso. Siempre siguiendosiendo humilde frente a la cienca pero prometiento hacer obra a su ritmo sin nunca resignarnos. Ahí está el increíble herencia que el fundador del Insa, Gaston Berger, nos lega. Más de un aspiración, es un deber hacia la sociedad que nos confia. Prospectiva y humanismo. Dos palabras por un ùnico modelo. Prospectiva para la preparación del futuro. Humanismo por nunca olvidar que la sola cosa que merece estar al corazón  del sistema es la humanidad ");
